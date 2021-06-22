281eb9c7a0b82e03c068f3c0fa31df78c3a2bed5
lancewburklin <1677@holbertonschool.com>
2021-06-22 17:27:46 -0500

    Adding esle if check

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -63,7 +63,8 @@ type Subjects = "Math" | "History";
 
 export function teachClass(todayClass:Subjects): string {
   if (todayClass === "Math") {
-    return ("Teaching Math");
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
   }
-  return ("Teaching History");
 }
