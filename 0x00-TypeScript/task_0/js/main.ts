0ad5923e9bdb9cb8669b6bcca197ed03c12866c9
lancewburklin <1677@holbertonschool.com>
2021-06-22 15:38:07 -0500

    Typing HTML Elements

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -21,13 +21,13 @@ const student2: Student = {
 
 const allStudents: Array<Student> = [student1, student2];
 
-const body = document.getElementsByTagName('body')[0];
-const table = document.createElement('table');
+const body: HTMLElement = document.getElementsByTagName('body')[0];
+const table: HTMLElement = document.createElement('table');
 for (let i = 0; i < allStudents.length; i++) {
-  const tblR = document.createElement('tr');
-  const name = document.createElement('td');
+  const tblR: HTMLElement = document.createElement('tr');
+  const name: HTMLElement = document.createElement('td');
   name.innerHTML = allStudents[i].firstName;
-  const location = document.createElement('td');
+  const location: HTMLElement = document.createElement('td');
   location.innerHTML = allStudents[i].location;
   tblR.appendChild(name);
   tblR.appendChild(location);
