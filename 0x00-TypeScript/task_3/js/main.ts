9f32b80a337d85a8b01757afe5a5cc1f6c145aaa
lancewburklin <1677@holbertonschool.com>
2021-06-22 18:49:56 -0500

    Adding task 3

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,15 @@
+/// <reference path="./crud.d.ts">
+import { RowID } from './interface';
+import { RowElement } from './interface';
+import * as CRUD from './crud';
+import { insertRow } from './crud';
+
+const row: RowElement = {
+  firstName: 'Guilaume',
+  lastName: 'Salva'
+};
+
+const newRowId: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowId, updatedRow);
+CRUD.deleteRow(newRowId);
