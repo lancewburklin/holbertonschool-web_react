6946f0887a670a64305d07e51d9e794f4e28219f
lancewburklin <1677@holbertonschool.com>
2021-06-22 15:31:28 -0500

    First typescript files

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,36 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+};
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 76,
+  location: "Tulsa",
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 77,
+  location: "Not Tulsa",
+};
+
+const allStudents = [student1, student2];
+
+const body = document.getElementsByTagName('body')[0];
+const table = document.createElement('table');
+for (let i = 0; i < allStudents.length; i++) {
+  const tblR = document.createElement('tr');
+  const name = document.createElement('td');
+  name.innerHTML = allStudents[i].firstName;
+  const location = document.createElement('td');
+  location.innerHTML = allStudents[i].location;
+  tblR.appendChild(name);
+  tblR.appendChild(location);
+  table.appendChild(tblR);
+};
+body.appendChild(table);
