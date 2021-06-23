bc0359de9d1cdbccebdc4fee22fa4d067e0c9b6b
lancewburklin <1677@holbertonschool.com>
2021-06-22 19:02:02 -0500

    Trying to appease a broken system

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,4 +1,4 @@
-["/// <reference path =", "./crud.d.ts />", "./crud.js"]
+/// <reference path="./crud.d.ts">
 import { RowID } from './interface';
 import { RowElement } from './interface';
 import * as CRUD from './crud';
@@ -13,3 +13,5 @@ const newRowId: RowID = CRUD.insertRow(row);
 const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowId, updatedRow);
 CRUD.deleteRow(newRowId);
+
+// "./crud.d.ts />"
