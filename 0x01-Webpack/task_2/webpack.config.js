72eec31419f5444eb3ed9e5e9eb99d057c06318b
lancewburklin <1677@holbertonschool.com>
2021-06-24 15:25:54 -0500

    Fixing comment

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         use: [
-          {loader: 'image-webpack-loader'}
+          {loader: 'image-webpack-loader'} //["file-loader"]
         ]
       }
     ]
