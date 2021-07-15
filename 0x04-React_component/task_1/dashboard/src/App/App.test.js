095659739b30391ded5fc01f24c5769556658005
lancewburklin <1677@holbertonschool.com>
2021-07-15 18:34:45 -0500

    Changing render to shallow

--- 0x04-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x04-React_component/task_1/dashboard/src/App/App.test.js
@@ -5,7 +5,7 @@
 
 import React from 'react'
 import '@testing-library/react'
-import { render, shallow, mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import App from './App.js'
 import CourseList from '../CourseList/CourseList'
 import CourseListRow from '../CourseList/CourseListRow.js'
@@ -13,27 +13,27 @@ import Login from '../Login/Login'
 
 
 test('Testing if App renders', () => {
-  const wrapper = render(<App />);
+  const wrapper = shallow(<App />);
   expect(wrapper).toMatchSnapshot;
 })
 
 test('Testing that Notifications is made', () => {
-  const wrapper = render(<App />);
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.Notifications')).not.toBe(null);
 })
 
 test('Testing that App-header is made', () => {
-  const wrapper = render(<App />);
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-header')).not.toBe(null);
 })
 
 test('Testing for App-body', () => {
-  const wrapper = render(<App />);
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-body')).not.toBe(null);
 })
 
 test('Testing for App-footer', () => {
-  const wrapper = render(<App />);
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-footer')).not.toBe(null);
 })
 
