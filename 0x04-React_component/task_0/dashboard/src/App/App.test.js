cb1189cec290f8335e0b4a3c8bb16a30a4b84835
lancewburklin <1677@holbertonschool.com>
2021-07-23 15:01:51 -0500

    Changing test with it

--- 0x04-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,42 +1,41 @@
 import React from 'react'
-import '@testing-library/react'
-import { render, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import App from './App.js'
 import CourseList from '../CourseList/CourseList'
 import CourseListRow from '../CourseList/CourseListRow.js'
 import Login from '../Login/Login'
 
-test('Testing if App renders', () => {
-  const wrapper = render(<App />);
+it('Testing if App redners', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper).toMatchSnapshot;
 })
 
-test('Testing that Notifications is made', () => {
-  const wrapper = render(<App />);
+it('Testing that Notifications is made', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.Notifications')).not.toBe(null);
 })
 
-test('Testing that App-header is made', () => {
-  const wrapper = render(<App />);
+it('Testing that App-header is made', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-header')).not.toBe(null);
 })
 
-test('Testing for App-body', () => {
-  const wrapper = render(<App />);
+it('Testing for App-body', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-body')).not.toBe(null);
 })
 
-test('Testing for App-footer', () => {
-  const wrapper = render(<App />);
+it('Testing for App-footer', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.find('.App-footer')).not.toBe(null);
 })
 
-test('Testing that CourseList is not displayed', () => {
+it('Testing that CourseList is not displayed', () => {
   const wrapper = shallow(<App />)
   expect(wrapper.find(CourseListRow)).toHaveLength(0);
 })
 
-test('Testing for CourseList', () => {
+it('Testing for CourseList', () => {
   const wrapper = shallow(<App isLoggedIn={true}/>)
   expect(wrapper.find(Login)).toHaveLength(0);
   expect(wrapper.find(CourseList)).toHaveLength(1);
