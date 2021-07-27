7c5ffe960bb6fa49881b750ef257cd4949caa0b4
lancewburklin <1677@holbertonschool.com>
2021-07-27 16:17:27 -0500

    Merge and concat a list

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,13 @@
+import { List } from 'immutable';
+
+export function concatElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+  return list1.concat(list2);
+}
+
+export function mergeElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+  return list2.merge(list1);
+}
