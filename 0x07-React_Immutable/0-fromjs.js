cf687826954e4161a5e1cd14920d3e0a1e78628f
lancewburklin <1677@holbertonschool.com>
2021-07-27 13:14:53 -0500

    Removed console log

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -2,11 +2,6 @@ const { fromJS } = require('immutable');
 
 function getImmutableObject(object) {
   return fromJS(object);
-};
+}
 
-console.log(getImmutableObject({
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}));
+export default getImmutableObject;
