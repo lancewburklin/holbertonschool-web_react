41993c9b27cc38ba356b04804f3ab14e2c3d27f4
lancewburklin <1677@holbertonschool.com>
2021-07-27 15:56:28 -0500

    Added semicolons

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,4 +1,4 @@
-import { getIn, fromJS } from 'immutable'
+import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   return getIn(fromJS(object), array);
