1a5d573b01448a1dceb08417ccab6d009dabb1e4
lancewburklin <1677@holbertonschool.com>
2021-08-05 13:04:05 -0500

    Task 6 commit

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,9 @@
+import {UNSELECT_COURSE, SELECT_COURSE} from './courseActionTypes'
+
+export function selectCourse(index) {
+  return { type: SELECT_COURSE, index: index };
+}
+
+export function unSelectCourse(index) {
+  return { type: UNSELECT_COURSE, index: index };
+}
